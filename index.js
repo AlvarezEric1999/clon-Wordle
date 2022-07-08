a4a951c10384eedf2ef22da175a6aad25f98321b
Eric Alvarez <alvarezaeric592@gmail.com>
2022-07-08 18:23:55 -0500

    agrego grilla

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+const grid = document.querySelector("#grid");
 const keyboard = document.querySelector("#keyboard");
 const keyboardLetters = [
 ["q","w","e","r","t","y","u","i","o","p"],
@@ -6,15 +12,34 @@ const keyboardLetters = [
 
 ];
 
+let attempt = 0;
+const row = [];
+
+for (let rows = 0; rows < 5 ; rows++) {
+    const list = document.createElement('ul');
+    list.classList.add("grid-row");
+    
+    for (let column = 0; column < 6; column++) {
+        const item = document.createElement('li')
+        item.classList.add("row-item");
+        item.id= `${rows}-${column}`;
+        list.appendChild(item)
+    }
+    
+ row.push(list)
+    
+}
+
+grid.append(...row)
+
+
 
 
 const listElement = [];
-
-const answer = [];
-
+let answer = [];
 const secretWord = ["p","l","a","t","z","i",];
+let positions= [];
 
-const positions= [];
 
 keyboardLetters.map((letters)=>{
 
@@ -57,22 +82,29 @@ keyboard.append(...listElement);
 
 const checkword =  () =>{
 
+
+if(attempt===5){
+
+    alert("ya no tienes mas intentos")
+    return;
+}
+
 if(answer.length===6){
+
+
+    attempt+=1;
     if(answer.join("") === secretWord.join("")){
+
+        alert("palabra correcta")
+
     } else{
-    
-    
         for (let i = 0 ; i < 6 ;i++){
-    
             switch(true){
-    
-    
                 case answer[i] === secretWord[i]:
                     positions.push("green");
     
                  break;
-                 
-                 
+                
                  case secretWord.includes(answer[i]):
                     positions.push("marron");
                  break;
@@ -80,14 +112,23 @@ if(answer.length===6){
                  default:
                     positions.push("gray")
                 break;
-    
-    
             }
-    
+            
         }
+        
+        positions.map((color, id)=>{
     
-        console.log(positions)
+            const item = document.getElementById(`${attempt}-1 -${id}`)
+            item.classList.add(color)
+    
+            console.log(item);
+           
+        })
+        
+        
     }
+
+
 
 
 }else{
@@ -97,7 +138,7 @@ if(answer.length===6){
 
 
 
-}
+};
 
 
 const deleteWord = () =>{
@@ -106,7 +147,7 @@ const deleteWord = () =>{
         alert("no tienes nada escrito")
     }
 
-   answer.pop
+   answer.pop();
    console.log(answer);
 }
 
@@ -115,22 +156,17 @@ const pressletter = () =>{
     const button = event.target
 
     if(answer.length<6){
+        const currenItem = document.getElementById(`${attempt}-${answer.length}`)
+        currenItem.textContent = button.textContent
         answer.push(button.id);
 
 
+    }else{
+
+        alert("tu palabra esta completa")
     }
 
-    console.log(answer);
 
 }
 
 
-const array = ["p","l","a","t","z","i"];
-
-for (let i = 0; i < array.length; i++) {
-     
-    const palabra = array[i]+array[i];
-
-    console.log(palabra);
-    
-}
